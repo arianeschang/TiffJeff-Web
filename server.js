19870624146ec9ca5fc64c646c660625d8a1cc7e
arianeschang <arianeschang@Arianes-MacBook-Pro.local>
2016-07-02 23:10:25 -0400

    Fixed Nav Bar (a bit)
    still trying to get the header photo to only appear in the default page

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
   , logger = require('morgan')
   , app = express()
-  , template = require('jade').compileFile(__dirname + '/source/templates/homepage.jade')
+  , template = require('jade').compileFile(__dirname + '/views/homepage.jade')
 
 app.use(logger('dev'))
 app.use(express.static(__dirname + '/static'))
@@ -14,6 +14,28 @@ app.get('/', function (req, res, next) {
     next(e)
   }
 })
+
+
+app.get('/', function (req, res, next) {
+  try {
+    var html = template({ title: 'Home' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/theBigDay', function (req, res, next) {
+  try {
+    bigDay = require('jade').compileFile(__dirname + '/views/theBigDay.jade')
+    var html = bigDay({ title: 'theBigDay' })
+    res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Listening on http://localhost:' + (process.env.PORT || 3000))
